3213c8fb467dd51cbd519c7fbd32688a30553cbd
Radmir Sataev <71800595+Xaktor@users.noreply.github.com>
2022-04-24 19:59:28 -0400

    adding code
    adding code

---
+++ website/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    })  
+    
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+
+//first typing animation
+/*var typed = new Typed(".typing", {
+  strings: ["WEBDEVSKI", "WEBDEVSKI", "WEBDEVSKI"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});*/
