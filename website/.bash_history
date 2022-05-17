78cc63161b689608f0b3247e956286d2dec58830
Radmir Sataev <71800595+Xaktor@users.noreply.github.com>
2022-05-17 17:13:44 -0400

    final product
    final presentation code

---
+++ website/.bash_history
@@ -0,0 +1,79 @@
+ls
+mkdir img
+ls
+cd img
+cd img/
+wget https://cdn.glitch.global/f824a37e-a4ec-4323-99c4-e7c1da4c18ae/Marinated-Mushrooms-Recipe-150.jpeg?v=1652630285113
+ls
+mv /app/img/Marinated-Mushrooms-Recipe-150.jpeg?v=1652630285113 /app/img/Marinated-Mushrooms-150.jpeg
+ls
+refresh
+cd img/
+ls
+mv /app/img/Marinated-Mushrooms-150.jpeg /app/img/Marinated-Mushrooms-Recipe-150.jpeg
+ls
+refresh
+ls
+cd i
+cd img
+wget https://cdn.glitch.global/f824a37e-a4ec-4323-99c4-e7c1da4c18ae/Marinated-Mushrooms-Recipe-150.webp?v=1652709308518
+mv /app/img/Marinated-Mushrooms-Recipe-150.webp?v=1652709308518 /app/img/Marinated-Mushrooms-Recipe-150.jpeg
+ls
+refresh
+manager/alo
+mkdir manager
+mkdir manager
+mkdir manager
+ls
+cd manager/
+ls
+mkfile hi.js
+touch grass.txt
+ls
+cd ..
+ls
+ls
+ls
+cd manager
+ls
+ls
+cd manager
+touch touch_grass.html
+ls
+ls
+refresh
+ls
+refresh
+cd manager
+touch yourmom.txt
+refresh
+touch createEmployee.html
+refresh
+touch createEmployee.js
+touch header.css
+touch index.html
+touch main.css
+touch main.js
+touch responsive.css
+refresh
+refresh
+npm install
+su npm
+npm
+npm install
+cd driver
+touch driver.js
+refresh
+refresh
+mkdir chef
+ls
+touch chef.html
+ls
+cd chef
+ls
+touch chef.html
+gedit chef.html
+clear
+ls
+ls
+refresh
--- website/script.js
+++ website/script.js
@@ -1,42 +1,40 @@
+
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }
-        else{
+            $('.navbar-order').addClass("sticky");
+        }else{
             $('.navbar').removeClass("sticky");
-        }
-    })  
-    
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
+            $('.navbar-order').removeClass("sticky");
         }
     });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
+  });
 });
 
 
-//first typing animation
-/*var typed = new Typed(".typing", {
-  strings: ["WEBDEVSKI", "WEBDEVSKI", "WEBDEVSKI"],
-  typeSpeed: 100,
-  backSpeed: 60,
-  loop: true,
-});*/
+
+
